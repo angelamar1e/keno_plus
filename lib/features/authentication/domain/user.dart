f5516cabb4e77f88618f22c266d5560e98984be2
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-30 14:13:07 +0800

    created user model

---
+++ lib/features/authentication/domain/user.dart
@@ -0,0 +1,17 @@
+class User {
+  final String username;
+  final String name;
+  final String email;
+  final String password;
+  final String phoneNumber;
+  final String address;
+
+  User({
+    required this.username,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    required this.address,
+  });
+}
