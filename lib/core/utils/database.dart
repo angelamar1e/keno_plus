7d97a3a9e34e8e9164dd447d94115186a79a03e6
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-05 13:00:10 +0800

    added get database method

--- lib/core/utils/database.dart
+++ lib/core/utils/database.dart
@@ -1,24 +1,34 @@
+import 'package:keno_plus/features/authentication/domain/models/user.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class AppDatabase {
+  // Singleton pattern to ensure only one instance of the database is created
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await initialize();
+    return _database!;
+  }
+
   static Future<Database> initialize() async {
     return openDatabase(
       join(await getDatabasesPath(), 'keno_plus.db'),
       onCreate: (db, version) {
-        return db.execute('''
-          CREATE TABLE users(
-            username TEXT PRIMARY KEY,
-            name TEXT,
-            email TEXT,
-            password TEXT,
-            phone TEXT,
-            birthdate TEXT,
-            age INTEGER,
-          )
-          ''');
+        return db.execute(
+          'CREATE TABLE users(username TEXT PRIMARY KEY NOT NULL, firstName TEXT, lastName TEXT, birthdate TEXT, age INTEGER, phoneNumber TEXT, email TEXT, password TEXT)',
+        );
       },
       version: 1,
+    );
+  }
+
+  static Future<void> insertUser(Database db, User user) async {
+    await db.insert(
+      'users',
+      user.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 }
