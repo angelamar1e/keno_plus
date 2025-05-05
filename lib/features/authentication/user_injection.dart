5470cf89ddb19865910560225a98d1a4279573c7
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-05 13:02:50 +0800

    created dependency injection

--- lib/features/authentication/user_injection.dart
+++ lib/features/authentication/user_injection.dart
@@ -1,1 +1,23 @@
+import 'package:keno_plus/core/utils/database.dart';
+import 'package:keno_plus/features/authentication/data/data_source/user_datasource.dart';
+import 'package:keno_plus/features/authentication/data/data_source/user_datasource_impl.dart';
+import 'package:get_it/get_it.dart';
+import 'package:keno_plus/features/authentication/data/repository/user_repository_impl.dart';
+import 'package:keno_plus/features/authentication/domain/repository/user_repository.dart';
+import 'package:keno_plus/features/authentication/domain/use_cases/create_user.dart';
 
+final GetIt sl = GetIt.instance;
+
+initAuthInjections() async {
+  // Initialize the database
+  final database = await AppDatabase().database;
+
+  // Register UserDataSource
+  sl.registerSingleton<UserDataSource>(UserDataSourceImpl(database));
+
+  // Register UserRepositoryImpl
+  sl.registerSingleton<UserRepository>(UserRepositoryImpl(sl()));
+
+  // Register CreateUser use case
+  sl.registerSingleton<CreateUser>(CreateUser(sl()));
+}
