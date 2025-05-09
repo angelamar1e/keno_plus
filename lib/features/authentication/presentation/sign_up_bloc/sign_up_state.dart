297091c2a31b4b4f74fb1b902eef82514d747991
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-09 17:28:28 +0800

    added a state per sign up field form

--- lib/features/authentication/presentation/sign_up_bloc/sign_up_state.dart
+++ lib/features/authentication/presentation/sign_up_bloc/sign_up_state.dart
@@ -4,27 +4,76 @@ enum SignUpStatus { initial, loading, success, failure }
 
 final class SignUpState {
   const SignUpState({
-    this.status = SignUpStatus.initial,
-    this.user,
-    this.errorMessage,
+    required this.firstName,
+    required this.lastName,
+    required this.username,
+    required this.isUsernameUnique,
+    required this.password,
+    required this.birthdate,
+    required this.age,
+    required this.phoneNumber,
+    required this.email,
+    required this.isSubmitting,
+    this.status,
+    required this.showError,
   });
 
-  final SignUpStatus status;
-  final UserModel? user;
-  final String? errorMessage;
-
-  bool get isSignedUp => status == SignUpStatus.success;
-  bool get isFailedSignUp => status == SignUpStatus.failure;
+  final Name firstName;
+  final Name lastName;
+  final Username username;
+  final bool isUsernameUnique;
+  final Password password;
+  final Birthdate birthdate;
+  final Age age;
+  final PhoneNumber phoneNumber;
+  final EmailAddress email;
+  final Either<Fail, UserModel>? status;
+  final bool isSubmitting;
+  final bool showError;
 
   SignUpState copyWith({
-    SignUpStatus? status,
-    UserModel? user,
-    String? errorMessage,
+    Name? firstName,
+    Name? lastName,
+    Username? username,
+    bool? isUsernameUnique,
+    Password? password,
+    Birthdate? birthdate,
+    Age? age,
+    PhoneNumber? phoneNumber,
+    EmailAddress? email,
+    Either<Fail, UserModel>? status,
+    bool isSubmitting = false,
+    bool showError = false,
   }) {
     return SignUpState(
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      username: username ?? this.username,
+      isUsernameUnique: isUsernameUnique ?? this.isUsernameUnique,
+      password: password ?? this.password,
+      birthdate: birthdate ?? this.birthdate,
+      age: age ?? this.age,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      email: email ?? this.email,
       status: status ?? this.status,
-      user: user ?? this.user,
-      errorMessage: errorMessage ?? this.errorMessage,
+      isSubmitting: isSubmitting,
+      showError: showError,
+    );
+  }
+
+  factory SignUpState.initial() {
+    return SignUpState(
+      firstName: Name(''),
+      lastName: Name(''),
+      username: Username(''),
+      isUsernameUnique: true,
+      password: Password(''),
+      birthdate: Birthdate(null),
+      age: Age(null),
+      phoneNumber: PhoneNumber(''),
+      email: EmailAddress(''),
+      isSubmitting: false,
+      showError: false,
     );
   }
 }
