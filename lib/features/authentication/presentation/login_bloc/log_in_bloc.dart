48d6990a31966f08fb8ba2d0f603684498dc22a1
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-13 17:14:24 +0800

    handled login form events

--- lib/features/authentication/presentation/login_bloc/log_in_bloc.dart
+++ lib/features/authentication/presentation/login_bloc/log_in_bloc.dart
@@ -1,10 +1,57 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:keno_plus/core/validation/auth_failure.dart';
+import 'package:keno_plus/features/authentication/data/models/password.dart';
+import 'package:keno_plus/features/authentication/data/models/user_model.dart';
+import 'package:keno_plus/features/authentication/data/models/username.dart';
+import 'package:keno_plus/features/authentication/domain/usecases/get_by_username_usecase.dart';
 
 part 'log_in_event.dart';
 part 'log_in_state.dart';
 
 class LogInBloc extends Bloc<LogInEvent, LogInState> {
-  LogInBloc() : super(LogInState()) {
-    on<LogInEvent>((event, emit) {});
+  final GetUserByUsername getUserByUsername;
+
+  LogInBloc({required this.getUserByUsername}) : super(LogInState.initial()) {
+    on<UsernameChanged>(_onUsernameChanged);
+    on<PasswordChanged>(_onPasswordChanged);
+    on<LoggingIn>(_onLoggingIn);
+  }
+
+  void _onUsernameChanged(
+    UsernameChanged event,
+    Emitter<LogInState> emit,
+  ) async {
+    emit(state.copyWith(username: Username(event.username)));
+  }
+
+  void _onPasswordChanged(PasswordChanged event, Emitter<LogInState> emit) {
+    emit(state.copyWith(password: Password(event.password)));
+  }
+
+  void _onLoggingIn(LoggingIn event, Emitter<LogInState> emit) async {
+    final isUsernameValid = state.username.value.isRight();
+    final isPasswordValid = state.username.value.isRight();
+
+    // Extract valid values
+    final String username = state.username.value.getOrElse(() => '');
+    final String password = state.password.value.getOrElse(() => '');
+
+    if (isUsernameValid && isPasswordValid) {
+      await Future.delayed(const Duration(seconds: 1));
+    }
+
+    // get user by username
+    final result = await getUserByUsername.call(username);
+
+    result.fold((authFail) => (emit(state.copyWith(status: Left(authFail)))), (
+      user,
+    ) {
+      if (user.password == password) {
+        emit(state.copyWith(status: Right(user)));
+      } else {
+        emit(state.copyWith(status: Left(AuthFailure.wrongPassword(password))));
+      }
+    });
   }
 }
