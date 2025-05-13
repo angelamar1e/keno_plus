bf0a208c3cae6eb209378e4ad41e2707618cdafc
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-13 16:00:40 +0800

    created login failure sub classes

--- lib/core/validation/auth_failure.dart
+++ lib/core/validation/auth_failure.dart
@@ -1,1 +1,22 @@
-class AuthFailure {}
+class AuthFailure {
+  final String? username;
+  final String? password;
+
+  const AuthFailure({this.username, this.password});
+
+  factory AuthFailure.userDoesNotExist(String username) {
+    return UserDoesNotExist(username);
+  }
+
+  factory AuthFailure.wrongPassword(String password) {
+    return WrongPassword(password);
+  }
+}
+
+class UserDoesNotExist extends AuthFailure {
+  const UserDoesNotExist(String? username) : super(username: username);
+}
+
+class WrongPassword extends AuthFailure {
+  const WrongPassword(String? password) : super(password: password);
+}
