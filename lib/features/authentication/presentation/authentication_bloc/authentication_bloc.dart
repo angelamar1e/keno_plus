ae48cb8e71500df4d2df20c733480388df9f1732
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-14 15:55:39 +0800

    added methods to persist auth locally

--- lib/features/authentication/presentation/authentication_bloc/authentication_bloc.dart
+++ lib/features/authentication/presentation/authentication_bloc/authentication_bloc.dart
@@ -1,25 +1,28 @@
-import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:keno_plus/core/utils/injections.dart';
 import 'package:keno_plus/features/authentication/data/models/user_model.dart';
+import 'package:keno_plus/features/authentication/domain/usecases/get_by_username_usecase.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(AuthenticationState()) {
+  AuthenticationBloc(this.getUser) : super(AuthenticationState()) {
     on<AuthenticationSucceeded>(_onAuthenticationSucceeded);
+    on<CheckAuthenticationStatus>(_onCheckAuthenticationStatus);
   }
+
+  final GetUserByUsername getUser;
 
   _onAuthenticationSucceeded(
     AuthenticationSucceeded event,
     Emitter<AuthenticationState> emit,
-  ) {
-    log(
-      'AuthenticationBloc instance in auth bloc : ${sl<AuthenticationBloc>().hashCode}',
-    );
+  ) async {
+    // Save the username in SharedPreferences
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('loggedInUser', event.user.username);
+
     emit(
       state.copyWith(
         status: AuthenticationStatus.authenticated,
@@ -27,5 +30,30 @@ class AuthenticationBloc
       ),
     );
     // set the logged in user in the shared preferences
+  }
+
+  Future<void> _onCheckAuthenticationStatus(
+    CheckAuthenticationStatus event,
+    Emitter<AuthenticationState> emit,
+  ) async {
+    // delay
+    await Future.delayed(const Duration(seconds: 2));
+
+    final prefs = await SharedPreferences.getInstance();
+    final username = prefs.getString('loggedInUser');
+
+    UserModel? user;
+    AuthenticationStatus status = AuthenticationStatus.unauthenticated;
+
+    if (username != null) {
+      final result = await getUser.call(username);
+
+      result.fold(
+        (fail) => {status = AuthenticationStatus.unauthenticated},
+        (user) => {status = AuthenticationStatus.authenticated, user = user},
+      );
+    }
+
+    emit(state.copyWith(status: status, user: user));
   }
 }
