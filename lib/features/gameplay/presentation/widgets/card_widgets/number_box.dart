3f0fec7ced1aa04ab8784198e396da3ef9f432c8
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-15 20:17:37 +0800

    created number box widget for cards

---
+++ lib/features/gameplay/presentation/widgets/card_widgets/number_box.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class NumberBox extends StatelessWidget {
+  final int number;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const NumberBox({
+    super.key,
+    required this.number,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 40,
+        height: 40,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.blue : Colors.grey[300],
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Text(
+          number.toString(),
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: isSelected ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
