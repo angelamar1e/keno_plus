e3ac5f405d470775a58d2007d008a83a552283a4
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-15 20:18:45 +0800

    renamed bloc

--- lib/features/gameplay/presentation/game_mode_selection_bloc/game_mode_selection_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'game_mode_selection_bloc.dart';
-
-class GameModeSelectionEvent {}
-
-class GameModeSelected extends GameModeSelectionEvent {
-  final GameMode gameModeSelected;
-
-  GameModeSelected({required this.gameModeSelected});
-}
