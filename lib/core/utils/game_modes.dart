b059d5a2a8712f61931346e164743affbe6cf2bf
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-05-15 20:16:50 +0800

    enhanced game mode enum

--- lib/core/utils/game_modes.dart
+++ lib/core/utils/game_modes.dart
@@ -1,1 +1,14 @@
-enum GameMode { classic, mini }
+enum GameMode {
+  classic(maxBets: 15, rows: 8, columns: 10),
+  mini(maxBets: 7, rows: 7, columns: 7);
+
+  final int maxBets;
+  final int rows;
+  final int columns;
+
+  const GameMode({
+    required this.maxBets,
+    required this.rows,
+    required this.columns,
+  });
+}
