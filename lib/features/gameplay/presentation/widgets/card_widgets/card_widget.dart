c5817fcb9bd24569aa0adf10bd436a648907a7ec
Angela <angela.delapena06@gmail.com>
2025-05-21 13:23:39 +0800

    minor formatting changes

--- lib/features/gameplay/presentation/widgets/card_widgets/card_widget.dart
+++ lib/features/gameplay/presentation/widgets/card_widgets/card_widget.dart
@@ -3,8 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:keno_plus/features/gameplay/presentation/card_bloc/card_bloc.dart';
 import 'package:keno_plus/features/gameplay/presentation/card_bloc/card_state.dart';
 import 'package:keno_plus/features/gameplay/presentation/widgets/card_widgets/number_box.dart';
-import 'package:keno_plus/features/gameplay/presentation/wager_bloc/wager_bloc.dart';
-import 'package:keno_plus/features/gameplay/presentation/wager_bloc/wager_state.dart';
 
 class CardWidget extends StatelessWidget {
   const CardWidget({
@@ -20,55 +18,49 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WagerBloc, WagerState>(
-      builder: (context, wagerState) {
-        return Column(
-          children: [
-            BlocListener<CardBloc, CardState>(
-              listener: (context, state) {
-                if (state.matchedBets.isNotEmpty) {
-                  // TODO: generate payout info
-                }
-              },
-              child: Center(
-                child: BlocBuilder<CardBloc, CardState>(
-                  builder: (context, state) {
-                    final bets = state.bets;
-                    final matches = state.matchedBets;
-                    final winningBets = state.winningBets;
-                    final bloc = context.read<CardBloc>();
+    return Column(
+      children: [
+        BlocListener<CardBloc, CardState>(
+          listener: (context, state) {
+            if (state.matchedBets.isNotEmpty) {
+              // generate payout info
+            }
+          },
+          child: Center(
+            child: BlocBuilder<CardBloc, CardState>(
+              builder: (context, state) {
+                final bloc = context.read<CardBloc>();
+                final bets = state.bets;
+                final matches = state.matchedBets;
+                final winningBets = state.winningBets;
 
-                    // Generate the grid of NumberBox widgets
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: columns,
-                        mainAxisSpacing: 8,
-                        crossAxisSpacing: 8,
-                      ),
-                      itemCount: numbersCount,
-                      itemBuilder: (context, index) {
-                        final number = index + 1; // Numbers start at 1
-                        return NumberBox(
-                          number: number,
-                          isWinningBet: winningBets.contains(number),
-                          isMatch: matches.contains(number),
-                          isSelected: bets.contains(number),
-                          onTap: () {
-                            bloc.add(
-                              BetsChanged(bet: number, maxBets: maxBets),
-                            );
-                          },
-                        );
+                // Generate the grid of NumberBox widgets
+                return GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: columns,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                  ),
+                  itemCount: numbersCount,
+                  itemBuilder: (context, index) {
+                    final number = index + 1; // Numbers start at 1
+                    return NumberBox(
+                      number: number,
+                      isWinningBet: winningBets.contains(number),
+                      isMatch: matches.contains(number),
+                      isSelected: bets.contains(number),
+                      onTap: () {
+                        bloc.add(BetsChanged(bet: number, maxBets: maxBets));
                       },
                     );
                   },
-                ),
-              ),
+                );
+              },
             ),
-          ],
-        );
-      },
+          ),
+        ),
+      ],
     );
   }
 }
