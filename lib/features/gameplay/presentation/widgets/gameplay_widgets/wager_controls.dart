7a9dd76dee6513580c35e5394e29ce509c628582
Angela <angela.delapena06@gmail.com>
2025-05-21 13:22:59 +0800

    provided singleton wager bloc

--- lib/features/gameplay/presentation/widgets/gameplay_widgets/wager_controls.dart
+++ lib/features/gameplay/presentation/widgets/gameplay_widgets/wager_controls.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:keno_plus/features/gameplay/gameplay_injections.dart';
 import 'package:keno_plus/features/gameplay/presentation/wager_bloc/wager_bloc.dart';
 import 'package:keno_plus/features/gameplay/presentation/wager_bloc/wager_event.dart';
 import 'package:keno_plus/features/gameplay/presentation/wager_bloc/wager_state.dart';
@@ -9,56 +10,59 @@ class WagerControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WagerBloc, WagerState>(
-      builder: (context, state) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Wager: \$${state.wager.toStringAsFixed(2)}',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Decrease wager button
-                IconButton(
-                  onPressed: () {
-                    context.read<WagerBloc>().add(DecreaseWager());
-                  },
-                  icon: const Icon(Icons.remove),
-                  tooltip: 'Decrease wager',
-                ),
-                // Halve wager button
-                IconButton(
-                  onPressed: () {
-                    context.read<WagerBloc>().add(WagerHalved());
-                  },
-                  icon: const Icon(Icons.exposure_neg_1),
-                  tooltip: 'Halve wager',
-                ),
-                // Double wager button
-                IconButton(
-                  onPressed: () {
-                    context.read<WagerBloc>().add(WagerDoubled());
-                  },
-                  icon: const Icon(Icons.exposure_plus_1),
-                  tooltip: 'Double wager',
-                ),
-                // Increase wager button
-                IconButton(
-                  onPressed: () {
-                    context.read<WagerBloc>().add(IncreaseWager());
-                  },
-                  icon: const Icon(Icons.add),
-                  tooltip: 'Increase wager',
-                ),
-              ],
-            ),
-          ],
-        );
-      },
+    return BlocProvider.value(
+      value: sl<WagerBloc>(),
+      child: BlocBuilder<WagerBloc, WagerState>(
+        builder: (context, state) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Wager: \$${state.wager.toStringAsFixed(2)}',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Decrease wager button
+                  IconButton(
+                    onPressed: () {
+                      context.read<WagerBloc>().add(DecreaseWager());
+                    },
+                    icon: const Icon(Icons.remove),
+                    tooltip: 'Decrease wager',
+                  ),
+                  // Halve wager button
+                  IconButton(
+                    onPressed: () {
+                      context.read<WagerBloc>().add(WagerHalved());
+                    },
+                    icon: const Icon(Icons.exposure_neg_1),
+                    tooltip: 'Halve wager',
+                  ),
+                  // Double wager button
+                  IconButton(
+                    onPressed: () {
+                      context.read<WagerBloc>().add(WagerDoubled());
+                    },
+                    icon: const Icon(Icons.exposure_plus_1),
+                    tooltip: 'Double wager',
+                  ),
+                  // Increase wager button
+                  IconButton(
+                    onPressed: () {
+                      context.read<WagerBloc>().add(IncreaseWager());
+                    },
+                    icon: const Icon(Icons.add),
+                    tooltip: 'Increase wager',
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
