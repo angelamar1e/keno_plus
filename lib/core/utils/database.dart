a8b0c004f492dcaf0729bf75f64dd5127a48bf31
Angela <angela.delapena06@gmail.com>
2025-05-26 10:29:06 +0800

    added queries for other creating other tables

--- lib/core/utils/database.dart
+++ lib/core/utils/database.dart
@@ -24,10 +24,33 @@ class AppDatabase {
         await db.execute('''
           CREATE TABLE game_history(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
             timestamp TEXT NOT NULL,
             game_mode TEXT NOT NULL,
             wager REAL NOT NULL,
-            card_payouts TEXT NOT NULL
+            FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE
+          )
+        ''');
+
+        // Create game_history table
+        await db.execute('''
+          CREATE TABLE tickets(
+            ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            game_history_id INTEGER,
+            winning_numbers TEXT NOT NULL,
+            spots TEXT NOT NULL,
+            catches TEXT NOT NULL,
+            payout REAL NOT NULL,
+            FOREIGN KEY(game_history_id) REFERENCES game_history(id) ON DELETE CASCADE
+          )
+        ''');
+
+        // Create user wallet table
+        await db.execute('''
+          CREATE TABLE wallet(
+            username TEXT PRIMARY KEY NOT NULL,
+            balance REAL NOT NULL,
+            FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE
           )
         ''');
       },
