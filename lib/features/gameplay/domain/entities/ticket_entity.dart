441bb47e7417e238e44d3345e9ae45fff640581e
Angela <angela.delapena06@gmail.com>
2025-05-27 09:57:08 +0800

    created ticket entity and model

---
+++ lib/features/gameplay/domain/entities/ticket_entity.dart
@@ -0,0 +1,17 @@
+class TicketEntity {
+  final String id;
+  final String gameHistoryId;
+  final String winningNumbers;
+  final String spots;
+  final String catches;
+  final double payout;
+
+  TicketEntity({
+    required this.id,
+    required this.gameHistoryId,
+    required this.winningNumbers,
+    required this.spots,
+    required this.catches,
+    required this.payout,
+  });
+}
