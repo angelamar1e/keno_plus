dbbed5e0791c2259aa998fa2227ed714900a6e30
Angela <angela.delapena06@gmail.com>
2025-05-27 10:45:46 +0800

    added use case for getting user's balance

---
+++ lib/features/wallet/domain/usecases/get_balance_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:keno_plus/features/wallet/domain/repository/wallet_repository.dart';
+
+class GetBalanceUsecase {
+  final WalletRepository walletRepository;
+
+  GetBalanceUsecase(this.walletRepository);
+
+  Future<Either<Fail, double>> call(String username) async {
+    return await walletRepository.getBalance(username);
+  }
+}
