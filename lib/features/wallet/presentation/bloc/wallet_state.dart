989e204b5662d73b44c3e460588900fefe8e0d1e
Angela <angela.delapena06@gmail.com>
2025-06-03 13:31:26 +0800

    changed wallet state to entity

--- lib/features/wallet/presentation/bloc/wallet_state.dart
+++ lib/features/wallet/presentation/bloc/wallet_state.dart
@@ -4,28 +4,26 @@ enum WalletStatus { initial, loading, loaded, error }
 
 class WalletState {
   final WalletStatus status;
-  final String username;
-  final double balance;
+  final WalletEntity? wallet;
   final String? errorMessage;
 
   const WalletState({
     required this.status,
+    required this.wallet,
     required this.errorMessage,
-    required this.username,
-    required this.balance,
   });
 
   factory WalletState.initial() {
     return const WalletState(
       status: WalletStatus.initial,
       errorMessage: null,
-      username: '',
-      balance: 0.0,
+      wallet: null,
     );
   }
 
   WalletState copyWith({
     WalletStatus? status,
+    WalletEntity? wallet,
     String? errorMessage,
     String? username,
     double? balance,
@@ -33,8 +31,7 @@ class WalletState {
     return WalletState(
       status: status ?? this.status,
       errorMessage: errorMessage ?? this.errorMessage,
-      username: username ?? this.username,
-      balance: balance ?? this.balance,
+      wallet: null,
     );
   }
 }
