735c7a9a86ec24b162b9be6ba37547d41e549e12
Angela <angela.delapena06@gmail.com>
2025-06-03 10:17:01 +0800

    reflect changes on wallet repositories

--- lib/features/wallet/data/repository/iwallet_repository.dart
+++ lib/features/wallet/data/repository/iwallet_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:keno_plus/features/wallet/data/datasources/wallet_datasource.dart';
+import 'package:keno_plus/features/wallet/data/models/wallet_model.dart';
+import 'package:keno_plus/features/wallet/domain/entities/wallet_entity.dart';
 import 'package:keno_plus/features/wallet/domain/repository/wallet_repository.dart';
 
 class IWalletRepository extends WalletRepository {
@@ -8,20 +10,21 @@ class IWalletRepository extends WalletRepository {
   IWalletRepository(this.walletDataSource);
 
   @override
-  Future<Either<Fail, void>> createWallet(String username) async {
-    return await walletDataSource.createWallet(username);
+  Future<Either<Fail<String>, WalletEntity>> createWallet(
+    WalletModel wallet,
+  ) async {
+    return await walletDataSource.createWallet(wallet);
   }
 
   @override
-  Future<Either<Fail, double>> getBalance(String username) async {
+  Future<Either<Fail<String>, double>> getBalance(String username) async {
     return await walletDataSource.getBalance(username);
   }
 
   @override
-  Future<Either<Fail, void>> updateBalance(
-    String username,
-    double amount,
+  Future<Either<Fail<String>, WalletEntity>> updateBalance(
+    WalletModel wallet,
   ) async {
-    return await walletDataSource.updateBalance(username, amount);
+    return await walletDataSource.updateBalance(wallet);
   }
 }
--- lib/features/wallet/domain/repository/wallet_repository.dart
+++ lib/features/wallet/domain/repository/wallet_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:keno_plus/features/wallet/data/models/wallet_model.dart';
+import 'package:keno_plus/features/wallet/domain/entities/wallet_entity.dart';
 
 abstract class WalletRepository {
-  Future<Either<Fail, void>> createWallet(String username);
-  Future<Either<Fail, double>> getBalance(String username);
-  Future<Either<Fail, void>> updateBalance(String username, double amount);
+  Future<Either<Fail<String>, WalletEntity>> createWallet(WalletModel wallet);
+  Future<Either<Fail<String>, double>> getBalance(String username);
+  Future<Either<Fail<String>, WalletEntity>> updateBalance(WalletModel wallet);
 }
